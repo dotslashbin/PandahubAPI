7303d9d5c227d6b50aefcbd47fff00a6a8f87fb6
dotslashbin <joshuarpf@gmail.com>
2024-10-20 13:20:55 -0400

    Created controller skeleton

---
+++ functions/src/controllers/eventController.js
@@ -0,0 +1,24 @@
+const { successResponse, errorResponse } = require('../utils/responseHelper');
+const eventQueryService = require('../services/read/eventQueryService');
+const eventCommandService = require('../services/write/eventCommandService');
+const { validateEvent } = require('../validators/eventValidator');
+
+exports.getAllEvents = async (req, res) => {
+    // TODO: implement
+};
+
+exports.getEventById = async (req, res) => {
+    // TODO: implement
+};
+
+exports.createEvent = async (req, res) => {
+    // TODO: implement
+};
+
+exports.updateEvent = async (req, res) => {
+    // TODO: implement
+};
+
+exports.deleteEvent = async (req, res) => {
+    // TODO: implement
+};
