37d417823348b92fff90ce3927911239ab81fb4a
dotslashbin <joshuarpf@gmail.com>
2024-10-21 12:32:54 -0400

    cleaned up

--- functions/index.js
+++ functions/index.js
@@ -1,11 +1,13 @@
-// /index.js
+// Imports
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
+
+// Initialize the app
 admin.initializeApp();
 
 const eventController = require('./src/controllers/eventController');
 
-// Define Cloud Functions for HTTP triggers
+// Cloud fuunction
 exports.getAllEvents = functions.https.onRequest(eventController.getAllEvents);
 exports.getEventById = functions.https.onRequest(eventController.getEventById);
 exports.createEvent = functions.https.onRequest(eventController.createEvent);
